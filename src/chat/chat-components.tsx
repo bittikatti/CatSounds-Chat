e4cc635cf8ea194027a643c75f9dc8c570c50932
bittikatti <28701834+bittikatti@users.noreply.github.com>
2026-04-13 19:15:02 +0300

    Move cursor back to textarea after send

--- src/chat/chat-components.tsx
+++ src/chat/chat-components.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import type { AssistantMessageProperty, UserMessageProperty } from './chat-types.tsx';
 
 type InputBoxProperties = {
@@ -7,9 +8,17 @@ type InputBoxProperties = {
 };
 
 export const InputBoxElement = ({ text, onTextChange, onSend }: InputBoxProperties) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const handleSend = ()=> {
+        onSend();
+
+        // move focus back to textarea
+        textareaRef.current?.focus();
+    }
     return (
         <div className="input-container">
             <textarea
+                ref={textareaRef} // To move cursor here after sending message
                 id='UserInput'
                 aria-label="Write your message here"
                 value={text}
@@ -20,7 +29,7 @@ export const InputBoxElement = ({ text, onTextChange, onSend }: InputBoxProperti
             <button
                 className='send-button'
                 aria-label='Send your message'
-                onClick={onSend}>
+                onClick={handleSend}>
                     {/* The icon is from here: https://icons.getbootstrap.com/icons/send-fill/ */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
